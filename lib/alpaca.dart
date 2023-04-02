bc2a8471a1f3c0b452a332a213154769425bae91
Steve Hamblett <steve.hamblett@linux.com>
2023-04-02 14:41:42 +0100

    Initial structure

---
+++ lib/alpaca.dart
@@ -0,0 +1,14 @@
+/*
+ * Package : alpaca
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 02/04/2023
+ * Copyright :  S.Hamblett
+ */
+
+library alpaca;
+
+import 'dart:ffi';
+import 'dart:typed_data';
+
+import 'package:ffi/ffi.dart' as ffi;
+// import 'src/implementation/alpaca_impl.dart' as mraaimpl;
