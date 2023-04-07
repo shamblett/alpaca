596843f76bb0524169c4a6c3550e65fc4c55a9ad
shamblett <steve.hamblett@linux.com>
2023-04-07 14:31:13 +0100

    Utils methods

--- lib/src/utils/api/alpaca_utils.dart
+++ lib/src/utils/api/alpaca_utils.dart
@@ -193,4 +193,97 @@ class AlpacaUtils {
       ..clear()
       ..addAll(subLogits);
   }
+
+  /// Sample next token given probabilities for each embedding
+  ///
+  ///   - consider only the top K tokens
+  ///   - from them, consider only the top tokens with cumulative probability > P
+  ///
+  static Id llamaSampleTopPTopK(
+      AlpacaGptVocab vocab,
+      double logits,
+      List<Id> lastNTokens,
+      double repeatPenalty,
+      int topK,
+      double toPp,
+      double temp) {
+
+
+    // int n_logits = vocab.id_to_token.size();
+    //
+    // std::vector<std::pair<double, gpt_vocab::id>> logits_id;
+    // logits_id.reserve(n_logits);
+    //
+    // {
+    // const double scale = 1.0/temp;
+    // for (int i = 0; i < n_logits; ++i) {
+    // // repetition penalty from CTRL paper (https://arxiv.org/abs/1909.05858)
+    // // credit https://github.com/facebookresearch/llama/compare/main...shawwn:llama:main
+    // if (std::find(last_n_tokens.begin(), last_n_tokens.end(), i) != last_n_tokens.end()) {
+    // // if score < 0 then repetition penalty has to multiplied to reduce the previous token probability
+    // if (logits[i] < 0.0) {
+    // logits_id.push_back(std::make_pair(logits[i]*scale*repeat_penalty, i));
+    // } else {
+    // logits_id.push_back(std::make_pair(logits[i]*scale/repeat_penalty, i));
+    // }
+    // } else {
+    // logits_id.push_back(std::make_pair(logits[i]*scale, i));
+    // }
+    // }
+    // }
+    //
+    // sample_top_k(logits_id, top_k);
+    //
+    // double maxl = -INFINITY;
+    // for (const auto & kv : logits_id) {
+    // maxl = std::max(maxl, kv.first);
+    // }
+    //
+    // // compute probs for the top K tokens
+    // std::vector<double> probs;
+    // probs.reserve(logits_id.size());
+    //
+    // double sum = 0.0;
+    // for (const auto & kv : logits_id) {
+    // double p = exp(kv.first - maxl);
+    // probs.push_back(p);
+    // sum += p;
+    // }
+    //
+    // // normalize the probs
+    // for (auto & p : probs) {
+    // p /= sum;
+    // }
+    //
+    // if (top_p < 1.0f) {
+    // double cumsum = 0.0f;
+    // for (int i = 0; i < (int) probs.size(); i++) {
+    // cumsum += probs[i];
+    // if (cumsum >= top_p) {
+    // probs.resize(i + 1);
+    // logits_id.resize(i + 1);
+    // break;
+    // }
+    // }
+    //
+    // cumsum = 1.0/cumsum;
+    // for (int i = 0; i < (int) probs.size(); i++) {
+    // probs[i] *= cumsum;
+    // }
+    // }
+    //
+    // //printf("\n");
+    // //for (int i = 0; i < (int) 10; i++) {
+    // //    printf("%d: '%s' %f\n", i, vocab.id_to_token.at(logits_id[i].second).c_str(), probs[i]);
+    // //}
+    // //printf("\n\n");
+    // //exit(0);
+    //
+    // std::discrete_distribution<> dist(probs.begin(), probs.end());
+    // int idx = dist(rng);
+    //
+    // return logits_id[idx].second;
+
+    return 0;
+  }
 }
